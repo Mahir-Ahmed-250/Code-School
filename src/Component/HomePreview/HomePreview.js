fd385dbca64c29520f423bab2d31fc3c5463ed2f
Mahir-Ahmed-250 <xahin.mahir@gmail.com>
2021-10-04 20:43:45 +0600

    Add Some Comments

--- src/Component/HomePreview/HomePreview.js
+++ src/Component/HomePreview/HomePreview.js
@@ -4,11 +4,12 @@ import { faDollarSign, faPlusCircle, faStar } from '@fortawesome/free-solid-svg-
 import './HomePreview.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// Icons
+const star = <FontAwesomeIcon icon={faStar} />
+const add = <FontAwesomeIcon icon={faPlusCircle} />
+const dollar = <FontAwesomeIcon icon={faDollarSign} />
 
 const HomePreview = (props) => {
-    const star = <FontAwesomeIcon icon={faStar} />
-    const add = <FontAwesomeIcon icon={faPlusCircle} />
-    const dollar = <FontAwesomeIcon icon={faDollarSign} />
     const { name, img, description, rating, instructor, price } = props.course
     return (
         <div className="col-md-6 col-lg">
@@ -21,7 +22,6 @@ const HomePreview = (props) => {
                         <p className='course-rating'>  {star}<span className='course-rating-number'> {rating}</span></p>
                         <p className='course-instructor'>   {instructor}</p>
                         <p className="course-price">Price: {dollar}{price}</p>
-
                         <Button variant="dark">{add} Enroll Now</Button>
                     </Card.Body>
                 </Card>
